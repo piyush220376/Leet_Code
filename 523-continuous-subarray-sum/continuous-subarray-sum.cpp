c9064c411b21fff8a03f7a3254c4674eb940c333
Piyush Aggarwal <piyushaggarwal220376@gmail.com>
2026-09-10 23:55:23 +0530

    Time: 79 ms (51.18%) | Memory: 146.4 MB (57.02%) - LeetSync

--- 523-continuous-subarray-sum/continuous-subarray-sum.cpp
+++ 523-continuous-subarray-sum/continuous-subarray-sum.cpp
@@ -1,27 +1,25 @@
-#include <vector>
-#include <unordered_map>
-using namespace std;
-
 class Solution {
 public:
     bool checkSubarraySum(vector<int>& nums, int k) {
-        unordered_map<int, int> map;
-        map[0] = -1; // To handle the case where the sub-array starts from index 0
-        int sum = 0;
-
-        for (int i = 0; i < nums.size(); i++) {
-            sum += nums[i];
-            int rem = sum % k;
-
-            if (map.find(rem) != map.end()) {
-                if (i - map[rem] > 1) {
-                    return true;
+int n=nums.size();
+        int prefixsum=0;
+        unordered_map<int,int> remain;
+        remain[0]=-1;
+        for(int i=0;i<n;i++){
+            prefixsum+=nums[i];
+            int rem=prefixsum%k;
+            if(rem<0){
+                rem+=k;
+            }
+            if(remain.find(rem)!=remain.end()){
+                if((i-remain[rem])>=2){
+                return true;
                 }
-            } else {
-                map[rem] = i;
+            }else{
+            remain[rem]=i;
             }
         }
-
         return false;
+
     }
 };
