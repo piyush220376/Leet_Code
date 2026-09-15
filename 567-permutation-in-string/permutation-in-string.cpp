7272f0dfb724fd722b0b48ed4f0befd14f81a392
Piyush Aggarwal <piyushaggarwal220376@gmail.com>
2026-09-15 12:57:43 +0530

    Time: 3 ms (66.00%) | Memory: 10.7 MB (26.93%) - LeetSync

---
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        unordered_map<char,int> freq1;
+        unordered_map<char,int> freq2;
+        for(int i=0;i<s1.length();i++){
+            freq1[s1[i]]++;
+        }
+        int left=0;
+        for(int right=0;right<s2.length();right++){
+            freq2[s2[right]]++;
+            if(right-left+1>s1.length()){
+                freq2[s2[left]]--;
+                if(freq2[s2[left]]==0){
+                    freq2.erase(s2[left]);
+                }
+                left++;
+            }
+            if(freq2==freq1){
+                return true;
+            }
+        }
+        return false;
+    }
+};
